72f1189b5a3fbb270c559d86b5fee2181e9c3369
键盘上的舞者 <Up@jianpanshangdewuzhedemacbook-pro.local>
2016-03-28 10:55:12 +0800

    Initial Commit

---
+++ Password/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Password
+//
+//  Created by 键盘上的舞者 on 3/28/16.
+//  Copyright © 2016 键盘上的舞者. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PasswordTests/PasswordTests.m
@@ -0,0 +1,39 @@
+//
+//  PasswordTests.m
+//  PasswordTests
+//
+//  Created by 键盘上的舞者 on 3/28/16.
+//  Copyright © 2016 键盘上的舞者. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PasswordTests : XCTestCase
+
+@end
+
+@implementation PasswordTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
